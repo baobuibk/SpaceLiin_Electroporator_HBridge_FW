6ea30e000ae217cbec06d97c0d7ef8e3baf4cc8f
HalmondD <duonganhkhoa2002@gmail.com>
2024-09-15 17:55:46 +0700

    update bin

--- Core/Src/stm32f0xx_it.c
+++ Core/Src/stm32f0xx_it.c
@@ -127,7 +127,7 @@ void PendSV_Handler(void)
 void SysTick_Handler(void)
 {
   /* USER CODE BEGIN SysTick_IRQn 0 */
-
+  SchedulerSysTickIntHandler();
   /* USER CODE END SysTick_IRQn 0 */
 
   /* USER CODE BEGIN SysTick_IRQn 1 */
